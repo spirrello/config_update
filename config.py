008a92e33d766245de5c721cbb9d7aacfdc25a2e
spirrell <spirrello@gmail.com>
2017-02-21 12:28:45 -0500

    Added flags for using telnet vs ssh and ios vs nx-os.

--- config.py
+++ config.py
@@ -3,9 +3,10 @@
 
 """
 
-import netmiko
+#import netmiko
 from netmiko import ConnectHandler
 import getpass
+import argparse
 
 #Configurations to push.
 #snmp_acl = 'SNMP-MGT'
@@ -15,29 +16,43 @@ snmp_location = 'snmp-server location LOCATION'
 snmp_contact = 'snmp-server contact someone@gmail.com'
 config_commands = [snmp_read,snmp_write,snmp_location,snmp_contact] 
 
+
+parser = argparse.ArgumentParser(
+       description='Script for pushing configuration changes.')
+
+parser.add_argument('-c', '--conn', required=True, action='store',
+                   help='Valid options are telnet or ssh.')
+
+parser.add_argument('-o', '--os', required=True, action='store',
+                   help='Identify IOS or NX-OS.')
+
+config_args = parser.parse_args()
+config_args.os = config_args.os.lower()
+
 username = input("Please enter username: ")
 password = getpass.getpass(prompt='Password: ', stream=None)
 
 file_name = input("Please enter the file name to read: ")
 
+#Load the file into memory as a list
 f = open(file_name, 'r')
-#f.read()
-
 ip_list =  list(f)
-
 f.close()
 
-print (ip_list)
-# for i in ip_list:
-#     print(i.rstrip())
 
+#List for devices we could not update.
 failed_list = []
 
 for ip in ip_list:
     try:
         print("\nConnecting to {0}".format(ip.rstrip()))
-        net_connect = ConnectHandler(device_type='cisco_ios', ip=ip.rstrip(),username=username,password=password,timeout=5)
-
+        #Needed logic for devices that have Telnet only vs SSH.
+        if config_args.conn == 'telnet' and config_args.os == 'ios':
+            net_connect = ConnectHandler(device_type='cisco_ios_telnet', ip=ip.rstrip(),username=username,password=password,timeout=5)
+        if config_args.conn == 'ssh' and config_args.os == 'ios':
+            net_connect = ConnectHandler(device_type='cisco_ios', ip=ip.rstrip(),username=username,password=password,timeout=5)
+        if config_args.conn == 'ssh' and config_args.os == 'nxos':
+            net_connect = ConnectHandler(device_type='cisco_nxos', ip=ip.rstrip(),username=username,password=password,timeout=5)
         output = net_connect.send_config_set(config_commands)
 
         print("Configuration sent....\n {0}".format(output))
@@ -55,13 +70,10 @@ for ip in ip_list:
         print("Error with {0}\n {1}".format(ip.rstrip(),e))
 
 if len(failed_list) > 0:
-    print("Configuration update has completed.  These devices however failed:")
+    print("Configuration update has completed.  These devices however failed:\n")
 
     for node in failed_list:
         print(node)
 else:
     print("Configuration update has completed.") 
-# parser = argparse.ArgumentParser(
-#        description='Reads file of ip addresses and pushes configurations.')
-# parser.add_argument('-f', '--file', required=False, action='store',
-#                  help='Name of file to read.')
+
